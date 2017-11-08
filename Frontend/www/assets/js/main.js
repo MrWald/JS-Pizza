f1f1e8e7b356cad8f86c8e881c5ee781f7c30c2a
Volodymyr <w260698@gmail.com>
2017-11-09 00:20:21 +0200

    Final
    Form validates input, style is fixed

--- Frontend/www/assets/js/main.js
+++ Frontend/www/assets/js/main.js
@@ -79,6 +79,59 @@ $(function(){
      $(item).text(num+" "+add);
  });
 
+ $("#flname").bind("keypress", function(event){
+     var regex= new RegExp("[А-Яа-яІіЇїЄєҐґ']");
+     var key=String.fromCharCode(!event.charCode ? event.which : event.charCode);
+     if(!regex.test(key)){
+         event.preventDefault();
+         return false;
+     }
+ });
+
+ $("#phone").bind("keypress", function(event){
+     var regex;
+     var key;
+     var text=$(this).val();
+     switch(text.length){
+         case 0:
+             regex=new RegExp("[0+]");
+             break;
+         case 1:
+             if(text.charAt(0)!=='0') {
+                 regex = new RegExp("[3]");
+                 break;
+             }
+         case 2:
+             if(text.charAt(0)!=='0') {
+                 regex = new RegExp("[8]");
+                 break;
+             }
+         case 3:
+             if(text.charAt(0)!=='0') {
+                 regex = new RegExp("[0]");
+                 break;
+             }
+         default:
+             regex = new RegExp("[0-9]");
+     }
+     key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+     if(!regex.test(key) || (text.charAt(0)==='0' && text.length===10) || (text.charAt(0)==='+' && text.length===13)){
+         event.preventDefault();
+         return false;
+     }
+ });
+
+ $("#address").bind("keypress", function(event){
+     var regex;
+     var key;
+     regex= new RegExp("^[0-9А-Яа-яІіЇїЄєҐґ'.,/ -]+$");
+     key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+     if(!regex.test(key)){
+         event.preventDefault();
+         return false;
+     }
+ });
+
  $("#submit").click(function () {
      var name = $("#flname").text();
      var phone = $("#phone").text();
